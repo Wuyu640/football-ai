b962f236b5c27cfe3fd24dc397ebb293d6218c1b
Wuyu640 <wenlianglin82@gmail.com>
2026-07-17 22:17:41 +0200

    Change background color to red and update heading

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,9 @@
 import { createRoot } from "react-dom/client";
 
+document.body.style.background = "red";
+
 createRoot(document.getElementById("root")).render(
-  <h1 style={{ color: "white", fontSize: "60px" }}>
-    FUNCIONA
+  <h1 style={{ color: "yellow", fontSize: "80px" }}>
+    PRUEBA 123
   </h1>
 );
