a11141c201d56cb73c754aefc70ed53e4391a95c
Wuyu640 <wenlianglin82@gmail.com>
2026-07-17 22:19:41 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,6 @@
 import { createRoot } from "react-dom/client";
-
-document.body.style.background = "red";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
-  <h1 style={{ color: "yellow", fontSize: "80px" }}>
-    PRUEBA 123
-  </h1>
+  <App />
 );
